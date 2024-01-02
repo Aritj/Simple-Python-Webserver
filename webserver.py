11510bdf1bf6e40b8d2086cd556c36847c9fb659
AriTJ <Ari.Joh20@student.setur.fo>
2024-01-02 00:18:20 +0000

    Improved the code documentation and added a query string exception handler

--- webserver.py
+++ webserver.py
@@ -3,21 +3,22 @@ import urllib.parse
 import json
 
 class HttpFields:
-    QUERY = "?"
-    QUERY_DELIMITER = "&"
-    DELIMITER = "."
-    ROUTE_DELIMITER = "/"
+    QUERY_DELIMITER = '?'
+    QUERY_KVP_DELIMITER = '&'
+    QUERY_KV_DELIMITER = '='
+    FILENAME_DELIMITER = '.'
+    ROUTE_DELIMITER = '/'
 
 class HttpServer:
-    DEFAULT_HOST = '0.0.0.0'
-    DEFAULT_PORT = 8000
+    DEFAULT_HOST: str = '0.0.0.0'
+    DEFAULT_PORT: int = 8000
 
-    def __init__(self, host: str = DEFAULT_HOST, port: int = DEFAULT_PORT):
+    def __init__(self, host: str = DEFAULT_HOST, port: int = DEFAULT_PORT) -> None:
         self.server_host = host
         self.server_port = port
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    def start(self):
+    def start(self) -> None:
         try:
             self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.server_socket.bind((self.server_host, self.server_port))
@@ -29,16 +30,18 @@ class HttpServer:
         finally:
             self.server_socket.close()
 
-    def handle_client(self, client_connection: socket.socket):
+    def handle_client(self, client_connection: socket.socket) -> None:
+        buffer_size: int = 1024
+        
         try:
-            request = client_connection.recv(1024).decode()
+            request = client_connection.recv(buffer_size).decode()
             response = self.process_request(request)
             client_connection.sendall(response.encode())
         finally:
             client_connection.close()
 
-    def process_request(self, request: str):
-        headers = request.split('\n')
+    def process_request(self, request: str) -> str:
+        headers: list[str] = request.split('\n')
         http_request_method, url, http_version = headers[0].split()
 
         if http_request_method not in ['GET']:
@@ -46,9 +49,12 @@ class HttpServer:
             # https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
             return self.format_http_response(501, f"{http_request_method} not implemented")
 
-        route, query_dict =  (url, {}) \
-            if url.find(HttpFields.QUERY) == -1 \
-            else self.parse_query_string(url)
+        try:
+            route, query_dict =  (url, {}) \
+                if url.find(HttpFields.QUERY_DELIMITER) == -1 \
+                else self.parse_query_string(url)
+        except:
+            return self.format_http_response(500, "Unable to parse query string.")
         
         if query_dict:
             # TODO: implement custom query string handling
@@ -62,19 +68,26 @@ class HttpServer:
 
     @staticmethod
     def parse_query_string(url_string: str) -> (str, dict[str, str]):
-        query_string = url_string[url_string.find(HttpFields.QUERY) + 1:]
-        pairs = query_string.split(HttpFields.QUERY_DELIMITER)
+        route_index: int = 0
+        query_string: str = url_string[url_string.find(HttpFields.QUERY_DELIMITER) + 1:]
+        pairs: list[str] = query_string.split(HttpFields.QUERY_KVP_DELIMITER)
 
-        query_dict = {}
+        query_dict: dict[str, str] = {}
         for pair in pairs:
             key, value = pair.split('=')
             query_dict[urllib.parse.unquote(key)] = urllib.parse.unquote(value)
 
-        return url_string.split(HttpFields.QUERY)[0], query_dict
+        return url_string.split(HttpFields.QUERY_DELIMITER)[route_index], query_dict
     
     @staticmethod
     def parse_route(route: str) -> (str, list[str], str):
-        return route if len(route[1:]) else 'index.html'
+        # change "/" to "index.html"
+        route = route if len(route[1:]) else 'index.html'
+        
+        # assume any route without file extension is ".html", else use provided file extension.
+        return route + ".html" \
+            if route.find(HttpFields.FILENAME_DELIMITER) == -1 \
+            else route
         
     @staticmethod
     def format_http_response(http_status_code: int, html: str) -> str:
