d98407f26261c512552c4cc0522c07e4f0f7843d
AriTJ <Ari.Joh20@student.setur.fo>
2024-01-02 23:17:59 +0100

    Refactored code by adding HttpStatus class.

--- webserver.py
+++ webserver.py
@@ -2,18 +2,34 @@ import socket
 import urllib.parse
 import json
 
+class HttpStatus:
+    OK: int = 200
+    BAD_REQUEST: int = 400
+    NOT_FOUND: int = 404
+    INTERNAL_SERVER_ERROR: int = 500
+    NOT_IMPLEMENTED = 501
+
+    def create_status_message(http_status_code: int, message: str):
+        http_version = 'HTTP/1.0'
+        # TODO: implement missing http_status_codes
+        # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+        match http_status_code:
+            case 200: return f'{http_version} 200 OK\n\n{message}'
+            case 400: return f'{http_version} 400 BAD REQUEST\n\n{message}'
+            case 404: return f'{http_version} 404 NOT FOUND\n\n{message}'
+            case 500: return f'{http_version} 500 INTERNAL SERVER ERROR\n\n{message}'
+            case 501: return f'{http_version} 501 NOT IMPLEMENTED\n\n{message}'
+
 class HttpFields:
     QUERY_DELIMITER = '?'
-    QUERY_KVP_DELIMITER = '&'
     QUERY_KV_DELIMITER = '='
+    QUERY_KVP_DELIMITER = '&'
     FILENAME_DELIMITER = '.'
     ROUTE_DELIMITER = '/'
 
 class HttpServer:
-    DEFAULT_HOST: str = '0.0.0.0'
-    DEFAULT_PORT: int = 8000
-
-    def __init__(self, host: str = DEFAULT_HOST, port: int = DEFAULT_PORT) -> None:
+    def __init__(self, host: str = '0.0.0.0', port: int = 8000) -> None:
         self.server_host = host
         self.server_port = port
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,7 +47,7 @@ class HttpServer:
             self.server_socket.close()
 
     def handle_client(self, client_connection: socket.socket) -> None:
-        buffer_size: int = 1024
+        buffer_size: int = 1024 # TODO: optimize this value
         
         try:
             request = client_connection.recv(buffer_size).decode()
@@ -42,32 +58,31 @@ class HttpServer:
 
     def process_request(self, request: str) -> str:
         headers: list[str] = request.split('\n')
-        http_request_method, url, http_version = headers[0].split()
+        http_request_method, url, _ = headers[0].split() # "GET /index.html HTTP/1.1"
 
         if http_request_method not in ['GET']:
-            # TODO: Handle HTTP request methods, e.g. POST, PUT
-            # https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
-            return self.format_http_response(501, f"{http_request_method} not implemented")
+            # TODO: Handle HTTP request methods, e.g. POST, PUT https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+            return HttpStatus.create_status_message(HttpStatus.NOT_IMPLEMENTED, f"{http_request_method} not implemented")
 
         try:
-            route, query_dict =  (url, {}) \
-                if url.find(HttpFields.QUERY_DELIMITER) == -1 \
-                else self.parse_query_string(url)
+            route, query_dict = self.parse_query_string(url)
         except:
-            return self.format_http_response(500, "Unable to parse query string.")
+            return HttpStatus.create_status_message(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to parse query string.")
         
         if query_dict:
-            # TODO: implement custom query string handling
             return json.dumps(query_dict, indent=4)
 
         try:
             with open(f'www/{self.parse_route(route)}', "r") as file:
-                return self.format_http_response(200, file.read())
+                return HttpStatus.create_status_message(HttpStatus.OK, file.read())
         except FileNotFoundError:
-            return self.format_http_response(404, "File Not Found")
+            return HttpStatus.create_status_message(HttpStatus.NOT_FOUND, "File Not Found")
 
     @staticmethod
     def parse_query_string(url_string: str) -> (str, dict[str, str]):
+        if url_string.find(HttpFields.QUERY_DELIMITER) == -1:
+            return url_string, {}
+
         route_index: int = 0
         query_string: str = url_string[url_string.find(HttpFields.QUERY_DELIMITER) + 1:]
         pairs: list[str] = query_string.split(HttpFields.QUERY_KVP_DELIMITER)
@@ -81,30 +96,15 @@ class HttpServer:
     
     @staticmethod
     def parse_route(route: str) -> (str, list[str], str):
-        # change "/" to "index.html"
-        route = route if len(route[1:]) else 'index.html'
+        if route == "/":
+            route = "index.html"
+
+        has_file_extension = route.find(HttpFields.FILENAME_DELIMITER) >= 0
         
         # assume any route without file extension is ".html", else use provided file extension.
-        return route + ".html" \
-            if route.find(HttpFields.FILENAME_DELIMITER) == -1 \
-            else route
-        
-    @staticmethod
-    def format_http_response(http_status_code: int, html: str) -> str:
-        _VERSION = 'HTTP/1.0'
-        # TODO: implement missing http_status_codes
-        # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-        match http_status_code:
-            case 200:
-                return f'{_VERSION} 200 OK\n\n{html}'
-            case 400:
-                return f'{_VERSION} 400 BAD REQUEST\n\n{html}'
-            case 404:
-                return f'{_VERSION} 404 NOT FOUND\n\n{html}'
-            case 500:
-                return f'{_VERSION} 500 INTERNAL SERVER ERROR\n\n{html}'
-            case 501:
-                return f'{_VERSION} 501 NOT IMPLEMENTED\n\n{html}'
+        return route \
+            if has_file_extension \
+            else route + ".html"
 
     
 if __name__ == '__main__':
